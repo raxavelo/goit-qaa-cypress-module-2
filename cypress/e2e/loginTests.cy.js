0dcc92b4f13c0d92f3647550cb9a468f12e8ff86
raxavelo <raxavelo@gmail.com>
2024-11-30 20:04:40 +0100

    Added Test 1

---
+++ cypress/e2e/loginTests.cy.js
@@ -0,0 +1,10 @@
+describe("Login tests", () => {
+  it("Test 1: Login without usage of self created command", () => {
+    cy.visit("https://www.edu.goit.global/account/login");
+    cy.get("#user_email").type("user888@gmail.com");
+    cy.get("#user_password").type("1234567890");
+    cy.get("button[type=submit]").click();
+    cy.get("#open-navigation-menu-mobile").click();
+    cy.get("button").contains("Log out").click();
+  });
+});
